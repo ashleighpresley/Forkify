b44bf88967d8a80d4a046ca40be8403c65dce155
ashleighpresley <ashleighpresley1@gmail.com>
2021-12-28 00:09:01 -0600

    Test removing fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,4 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 import View from './View';
 
 class RecipeView extends View {
@@ -129,7 +128,8 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? ing.quantity.toString() : ''
+      // ing.quantity ? new Fraction(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
