6135cf01dfcf807288414ce7b96d82b03ea06462
ashleighpresley <ashleighpresley1@gmail.com>
2021-12-27 23:11:10 -0600

    Adding documentation

--- dist/index.05cf099e.js
+++ dist/index.05cf099e.js
@@ -14364,7 +14364,14 @@ var _iconsSvg = require("url:../../img/icons.svg");
 var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
 class View {
     _data;
-    render(data, render = true) {
+    /**
+   * Render the received object to the DOM
+   * @param {Object | Obejct []} data The data to be rendered (e.g. a recipe)
+   * @param {boolean} [render = true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Ashleigh Presley
+   */ render(data, render = true) {
         if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
         this._data = data;
         const markup = this._generateMarkup();
